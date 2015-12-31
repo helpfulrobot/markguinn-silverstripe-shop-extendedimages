ad07f2438c484a99d003af6dbce7beb027aeb818
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:14:47 +1300

    Converted to PSR-2

--- code/MultipleProductImages.php
+++ code/MultipleProductImages.php
@@ -9,43 +9,47 @@
  */
 class MultipleProductImages extends DataExtension
 {
-	private static $many_many = array(
-		'AdditionalImages' => 'Image',
-	);
+    private static $many_many = array(
+        'AdditionalImages' => 'Image',
+    );
 
-	private static $many_many_extraFields = array(
-		'AdditionalImages' => array(
-			'SortOrder' => 'Int',
-		),
-	);
+    private static $many_many_extraFields = array(
+        'AdditionalImages' => array(
+            'SortOrder' => 'Int',
+        ),
+    );
 
-	/**
-	 * @param FieldList $fields
-	 */
-	function updateCMSFields(FieldList $fields) {
-		$fields->addFieldsToTab('Root.Images', array(
-			new SortableUploadField('AdditionalImages', _t('SHOPEXTENDEDIMAGES.AdditionImages', 'Additional Images')),
-			new LiteralField('additionalimagesinstructions', '<p>' . _t('SHOPEXTENDEDIMAGES.Instructions', 'You can change the order of the Additional Images by clicking and dragging on the image thumbnail.') . '</p>'),
-		));
-	}
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldsToTab('Root.Images', array(
+            new SortableUploadField('AdditionalImages', _t('SHOPEXTENDEDIMAGES.AdditionImages', 'Additional Images')),
+            new LiteralField('additionalimagesinstructions', '<p>' . _t('SHOPEXTENDEDIMAGES.Instructions', 'You can change the order of the Additional Images by clicking and dragging on the image thumbnail.') . '</p>'),
+        ));
+    }
 
-	/**
-	 * Combines the main image and the secondary images
-	 * @return ArrayList
-	 */
-	function AllImages() {
-		$list = new ArrayList($this->owner->AdditionalImages()->sort('SortOrder')->toArray());
-		$main = $this->owner->Image();
-		if ($main && $main->exists()) $list->unshift($main);
-		return $list;
-	}
+    /**
+     * Combines the main image and the secondary images
+     * @return ArrayList
+     */
+    public function AllImages()
+    {
+        $list = new ArrayList($this->owner->AdditionalImages()->sort('SortOrder')->toArray());
+        $main = $this->owner->Image();
+        if ($main && $main->exists()) {
+            $list->unshift($main);
+        }
+        return $list;
+    }
 
-	/**
-	 * @return DataList
-	 */
-	function SortedAdditionalImages() {
-		$list = $this->owner->AdditionalImages()->sort('SortOrder');
-		return $list;
-	}
-
+    /**
+     * @return DataList
+     */
+    public function SortedAdditionalImages()
+    {
+        $list = $this->owner->AdditionalImages()->sort('SortOrder');
+        return $list;
+    }
 }
